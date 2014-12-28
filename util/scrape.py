1a7735b2b83d62adc5b2e6584c8afca073d76751
Zack Mullaly <redwire@riseup.net>
2014-12-28 17:05:54 -0330

    Changed scrape tool to produce reports like the json files that exist now

--- util/scrape.py
+++ util/scrape.py
@@ -5,10 +5,10 @@ from json import dumps as to_json
 
 import sys
 
-dept, url = sys.argv[1:3]
+dept, name, url = sys.argv[1:4]
 soup = make_soup(get(url).read())
 courses = soup.findAll('div', attrs={'class': 'course'})
-report = {}
+report = {'name': name, 'identifier': dept, 'courses': {}}
 
 for course in courses:
   courseid = course.previousSibling.previousSibling['name'].strip()
@@ -22,7 +22,7 @@ for course in courses:
   except:
     desc = 'No description available.'
   attrs = course.findAll('p', attrs={'class': 'courseAttrs'})
-  report[courseid] = {
+  report['courses'][courseid] = {
     'courseNumber': number,
     'courseTitle': title,
     'courseDescription': desc,
@@ -41,11 +41,11 @@ for course in courses:
         val = link['onclick'].split()[0]
         ids.append(val[val.index('\'') + 1 : -2])
       if k == 'PR':
-        report[courseid]['prerequisites'] = ids
+        report['courses'][courseid]['prerequisites'] = ids
       elif k == 'CR':
-        report[courseid]['concurrent'] = ids
+        report['courses'][courseid]['concurrent'] = ids
       elif k == 'CO':
-        report[courseid]['corequisites'] = ids
-    report[courseid][k] = v
+        report['courses'][courseid]['corequisites'] = ids
+    report['courses'][courseid][k] = v
 
-print to_json({dept: report}, indent=4)
+print to_json(report, indent=4)
