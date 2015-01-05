e851636a44ec05662345fe603733eb0fc1330245
Zack Mullaly <redwire@riseup.net>
2015-01-05 12:03:10 -0330

    Connect to the courses database and route to appropriate handlers

--- app/app.js
+++ app/app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sqlite = require('sqlite');
 
+var config = require('./config');
 var routes = require('./routes/index');
+var courses = require('./routes/courses');
 
 var app = express();
+var db = new sqlite.Database();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +26,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+db.open(config.coursesDB, function (error) {
+  if (error) {
+    console.log('Could not connect to ' + config.coursesDB);
+    app.use('/courses', courses(null));  // TODO: Handle no db connection
+  } else {
+    app.use('/courses', courses(db));
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
